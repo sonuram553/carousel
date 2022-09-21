6c4248b4fc5260e638acabc1686f6d2c8ae0e471
Sonu Ram <sonuram6960@gmail.com>
2022-09-21 21:51:57 +0530

    Add background colors to item and body

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,13 @@ export default function App() {
   return (
     <div className="app">
       <Carousel itemMarginRight={10} itemHorizontalBorderWidth={2}>
+        <div className="item item-200">200</div>
+        <div className="item item-200">200</div>
         <div className="item item-50">50</div>
         <div className="item item-50">50</div>
         <div className="item item-50">50</div>
+        <div className="item item-300">300</div>
+        <div className="item item-300">300</div>
         <div className="item item-100">100</div>
         <div className="item item-100">100</div>
         <div className="item item-100">100</div>
