ae23c11fca0da1bf38fb94833a44771afece5f2a
Sonu Ram <sonuram6960@gmail.com>
2022-09-21 21:52:30 +0530

    Remove item borders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Carousel from "./carousel/Carousel";
 export default function App() {
   return (
     <div className="app">
-      <Carousel itemMarginRight={10} itemHorizontalBorderWidth={2}>
+      <Carousel itemMarginRight={10} itemHorizontalBorderWidth={0}>
         <div className="item item-200">200</div>
         <div className="item item-200">200</div>
         <div className="item item-50">50</div>
